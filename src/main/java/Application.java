3d36e7cc77ec95aed364428cb79cb90caf9e8131
eligoodwin <goodwiel@oregonstate.edu>
2018-05-27 16:41:42 -0700

    should work now, will make keys static now

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,23 +11,23 @@ import java.security.spec.X509EncodedKeySpec;
 public class Application {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, UnsupportedEncodingException, InvalidKeySpecException {
-        AssymEncypt assymEncypt = new AssymEncypt();
         AssymEncypt assymEncypt1 = new AssymEncypt();
+        AssymEncypt assymEncypt2 = new AssymEncypt();
 
-        String publicKey1 = assymEncypt.getPublicKeyString();
-        String publicKey2 = assymEncypt1.getPublicKeyString();
+        String publicKey1 = assymEncypt1.getPublicKeyString();
+        String publicKey2 = assymEncypt2.getPublicKeyString();
         System.out.printf("KEY %s\n", publicKey1);
         System.out.printf("KEY2 %s\n", publicKey2);
         String message = "this is java encryption";
 
 
-        String encryptedMessage = assymEncypt.encryptString(message, assymEncypt.makePublicKeyFromString(publicKey2));
-        String encryptedMessage1 = assymEncypt1.encryptString(message, assymEncypt1.makePublicKeyFromString(publicKey1));
+        String encryptedMessage = assymEncypt1.encryptString(message, assymEncypt1.makePublicKeyFromString(publicKey2));
+        String encryptedMessage1 = assymEncypt2.encryptString(message, assymEncypt2.makePublicKeyFromString(publicKey1));
         System.out.printf("Message 1: %s\n", encryptedMessage);
         System.out.printf("Message 2: %s\n", encryptedMessage1);
 
-        System.out.println(assymEncypt.decryptString(encryptedMessage1));
-        System.out.println(assymEncypt1.decryptString(encryptedMessage));
+        System.out.println(assymEncypt1.decryptString(encryptedMessage1));
+        System.out.println(assymEncypt2.decryptString(encryptedMessage));
 
 
     }
